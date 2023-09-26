f5a7cdec2cf4376c5cdc4357da84ec65dee66188
its zaki <zakiyounus789@gmail.com>
2023-09-26 22:08:02 +0500

    To Do List

--- app.js
+++ app.js
@@ -9,27 +9,38 @@ function todo(e){
     }
     else{
     e.preventDefault();
+    // const ul = createElement("UL")
+    // ul.appendChild("li") 
     let li = document.createElement('LI')
     let text = document.createTextNode(input.value)
     li.appendChild(text)
     ul.appendChild(li)
+    li.setAttribute("class", "li-div")
     const Delete = createElement("button" , "Delete")
-    li.appendChild(Delete)
+    // li.appendChild(Delete)
     Delete.setAttribute("onclick", "Delete(this)")
+    Delete.setAttribute("class", "del")
     const edit = createElement("button" , "Edit")
     edit.setAttribute("onclick", "edit(this)")
-    li.appendChild(edit)
-    }
+    edit.setAttribute("class", "edit")
+    // li.appendChild(edit)
+    const div = createElement("DIV", "")
+    div.appendChild(Delete)
+    div.appendChild(edit)
+    li.appendChild(div)
+    input.value =""
+}
 
 }
 function Delete(element){
-    ul.removeChild(element.parentNode)
+    // console.log(element.parentNode.parentNode);
+    ul.removeChild(element.parentNode.parentNode)
 
 }
 function edit(element){
     const edited = prompt("Enter Your Task")
     if(edited != ""){
-        element.parentNode.childNodes[0].nodeValue = edited
+        element.parentNode.parentNode.childNodes[0].nodeValue = edited
     }
     else{
         alert("Enter Any Text")
